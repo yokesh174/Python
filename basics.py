7c2db655d22996d98ca27156ae31600df4e77d98
Yokesh B <143736575+yokesh174@users.noreply.github.com>
2024-02-18 23:09:39 +0530

    Update and rename demo.py to basics.py

---
+++ basics.py
@@ -0,0 +1,44 @@
+PYTHON PROGRAMMING
+
+--->VARIABLES
+             Variables in Python are symbolic names that refer to objects in memory
+Ex:
+   a=10
+   b=10
+   print(a+b)
+output:
+   20
+Here the a and b are the variables
+
+--->DATATYPES 
+            Data types are the classification or categorization of data items. 
+            It represents the kind of value that tells what operations can be performed on a particular data
+
+Ex 1:
+   a=10
+   print(type(a))
+output:
+     <class 'int'>
+
+Ex 2:
+   a="yoki"
+   print(type(a))
+output:
+    <class 'str'>
+
+like that there are so many datatypes are there such as integer , string, float etc....
+
+
+--->OPERATORS
+            Operators are used to perform operations on variables and values.
+
+Ex:
+
+  a=10
+  b=20
+  c=a+b
+  print(c)
+output:
+    30
+
+here the addtion symbol is an opertor. like that so many operators are there such as +,-,*,% etc..
